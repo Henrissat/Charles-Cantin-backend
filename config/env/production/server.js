77eefa54746919d1e841ba0daf9cfe468052297a
Mathieu <studio.creatic@gmail.com>
2022-03-09 22:36:32 +0100

    server config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
     url: env('https://backend-charles-cantin.herokuapp.com'),
     proxy: true,
     app: {
-        keys: env.array("APP_KEYS", ["TEST"]),
+        keys: env.array("APP_KEYS", ["9TJpN/qHlNeciq/Xp22i0w==","3t1DrhCe4WQo0MYGEHzCtg==","r5rAdJ0Vs1XjsWQtvsDCPA==","30gzTlfbLw7dCoOM5scYyQ=="]),
     },
   });
