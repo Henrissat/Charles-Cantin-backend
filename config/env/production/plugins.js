eaa6e6e2edc0f94564518ae61f1f327b1dbddda7
Mathieu <studio.creatic@gmail.com>
2022-03-14 11:20:48 +0100

    correction bug test 1 provider.init

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,16 +1,11 @@
-module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('doe4mucdz'),
-        api_key: env('992987985328476'),
-        api_secret: env('g2OSMsPS9hjVe0gNH6jNPreeADw'),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
-    },
-    // ...
-  });
+module.exports = ({ env }) => ({ 
+  upload: { 
+    config:{ 
+      provider: 'cloudinary', 
+      providerOptions: { cloud_name: env('CLOUDINARY_NAME'), 
+      api_key: env('CLOUDINARY_KEY'), 
+      api_secret: env('CLOUDINARY_SECRET'), 
+      }, 
+    }, 
+  },
+})
